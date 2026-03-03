6f89a7757faf68ab7f448fd96e077200d2bacd23
mudash-dev <ashleymudambaji@gmail.com>
2026-03-03 11:52:25 +0300

    Feature App: Fix locked project files error

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
+const path = require('path');
+
 const nextConfig: NextConfig = {
   /* config options here */
+  turbopack: {
+    // Set the root to the directory where next.config.js is located
+    // or a parent directory if Next.js is installed higher up.
+    root: path.join(__dirname, '..'), 
+  },
 };
 
 export default nextConfig;
